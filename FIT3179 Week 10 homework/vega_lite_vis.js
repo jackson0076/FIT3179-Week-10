da8d5d06e0af15a1aa01a4d008152639921579d5
jackson0076 <jngu0076@student.monash.edu>
2024-10-06 22:39:36 +1100

    Push week 10 content

---
+++ FIT3179 Week 10 homework/vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "rainfall_map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2= "homicide_graph.vg.json";
+vegaEmbed("#line_graph", vg_2).then(function(result) {
+}).catch(console.error);
