06f5da5c624526bc05b8f262d57a4ecaed13efe7
jackson0076 <jngu0076@student.monash.edu>
2024-10-14 14:40:52 +1100

    added 4 other graphs as well as datasets. need to add annotation and more designs

--- Assignment 2/vega_lite_vis.js
+++ Assignment 2/vega_lite_vis.js
@@ -1,3 +1,20 @@
 var vg_1 = "chloro_map.vg.json";
-vegaEmbed("#chloro_map", vg_1).then(function(result) {
+vegaEmbed("#chloro_map", vg_1, { "actions": false }).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "line_graph.vg.json";
+vegaEmbed("#line_graph", vg_2, { "actions": false }).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_3, { "actions": false }).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "layered_bar.vg.json";
+vegaEmbed("#layered_bar", vg_4, { "actions": false }).then(function(result) {
+}).catch(console.error);
+
+
+var vg_5 = "scatter.vg.json";
+vegaEmbed("#scatter_plot", vg_5, { "actions": false }).then(function(result) {
 }).catch(console.error);
